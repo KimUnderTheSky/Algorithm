17bf075aef00d5864439fb902d53b0a16ef14a5a
cheonha <cjsgksla@ajou.ac.kr>
2023-06-23 20:43:46 +0900

    그룹 에너그램 풀이

--- StringManipulation/그룹 애너그램/GroupAnagrams.py
+++ StringManipulation/그룹 애너그램/GroupAnagrams.py
@@ -11,14 +11,17 @@ from typing import List
 words = ["eat","tea","tan", "ate","nat","bat"] 
 sorted_words = []
 
-def groupAnagrams(strs: List[str]) -> List[List[str]]: # 리스트 입력, 딕셔너리 출력
+def groupAnagrams(strs: List[str]) -> List[List[str]]: # 리스트 입력, 리스트 안에 리스트가 들어있는 자료형 출력
     anagrams = collections.defaultdict(list)
 
         # List
     for word in strs:
-        # 정렬하여 딕셔너리에 추가
-        anagrams[''.join(sorted(word))].append(word)
+        # 정렬하여 리스트에 추가
+        anagrams[''.join(sorted(word))].append(word) 
+        # anagram은 딕셔너리. 정렬된 word를 키로 하여 word를 삽입, sorted메서드 사용 시 문자열을 리스트로 반환하기 때문에 ''문자열에 join()시키면서 다시 문자열로 결합할 수 있다.
+        # sorted와 다르게 .sort()메서드는 제자리 정렬을 함으로 None을 반환
     return list(anagrams.values())
 
-
+anagrams = groupAnagrams(words)
+print(anagrams)
 # 풀이중
